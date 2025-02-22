67c27385f14d32b21a5193db6dd43bde1edb0752
MACHINTOSH <machintosh@Miracle.local>
2025-02-22 22:47:46 +0100

    some changes made to images

--- lib/cores/utils/image_constants.dart
+++ lib/cores/utils/image_constants.dart
@@ -1,16 +1,26 @@
-
-class ImageConstants{
+class ImageConstants {
   // Image folder path
   static String imagePath = 'assets/images';
 
   // headphone path
   static String headphonePath = 'assets/images/headphones';
 
+  // smartphone path
+  static String smartwatchPath = 'assets/images/smartwatch';
+
+  
   // common images
   static String camImg = '$imagePath/camm.png';
 
   // headphone images
- static String headphoneImg = '$headphonePath/h2.png';
+  static String headphoneImg = '$headphonePath/h2.png';
 
+  // splash screen images
+  static String splashscreenImage = '$imagePath/electronics-computer-thumbnail-removebg-preview.png';
 
+// phone images
+  static String phoneImage = '$imagePath/phone.png';
+
+// smartwatch images
+  static String smartwatchImage = '$smartwatchPath/watchsix-removebg-preview.png';
 }
--- lib/presentation/onboarding/onboarding2.dart
+++ lib/presentation/onboarding/onboarding2.dart
@@ -1,4 +1,5 @@
 import 'package:electronicsshop_app/cores/app_exports.dart';
+import 'package:electronicsshop_app/cores/utils/image_constants.dart';
 
 class Onboarding2 extends StatefulWidget {
   final Function switchScreen;
@@ -17,40 +18,72 @@ class _Onboarding2State extends State<Onboarding2> {
           padding: const EdgeInsets.all(30.0),
           child: Column(
             children: [
-              const SizedBox(height: 20,),
-              const Center(child: Text("Welcome to Electro-Store", style: TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),)),
-              const SizedBox(height: 30,),
-              const BuildSections(image: "assets/images/phone.png", title: "Smartphone X", text: "Experience the latest technology in your hand with cutting-edge features and design"),
-              const SizedBox(height: 30,),
-              BuildSections(image: ImageConstants.headphoneImg, title: "Wireless Headphones", text: "Immerse yourself in music with noise-cancellation and superior sound quality"),
-              const SizedBox(height: 30,),
-              const BuildSections(image: "assets/images/smartwatch/watchsix-removebg-preview.png", title: "Smartwatch Pro", text: "Stay connected and track your health with this efficient and stylish smartwatch"),
-              const SizedBox(height: 30,),
+              const SizedBox(
+                height: 20,
+              ),
+              const Center(
+                  child: Text(
+                "Welcome to Electro-Store",
+                style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              )),
+              const SizedBox(
+                height: 30,
+              ),
+              BuildSections(
+                  image: ImageConstants.phoneImage,
+                  title: "Smartphone X",
+                  text:
+                      "Experience the latest technology in your hand with cutting-edge features and design"),
+              const SizedBox(
+                height: 30,
+              ),
+              BuildSections(
+                  image: ImageConstants.headphoneImg,
+                  title: "Wireless Headphones",
+                  text:
+                      "Immerse yourself in music with noise-cancellation and superior sound quality"),
+              const SizedBox(
+                height: 30,
+              ),
+              BuildSections(
+                  image: ImageConstants.smartwatchImage,
+                  title: "Smartwatch Pro",
+                  text:
+                      "Stay connected and track your health with this efficient and stylish smartwatch"),
+              const SizedBox(
+                height: 30,
+              ),
               SizedBox(
                 width: 500,
                 child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(backgroundColor: Colors.pink),
-                    onPressed: (){
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Colors.pink),
+                    onPressed: () {
                       NavigatorService.pushNamed(AppRoutes.auth);
                     },
-                    child: const Text("Next", style: TextStyle(color: Colors.white),)),
+                    child: const Text(
+                      "Next",
+                      style: TextStyle(color: Colors.white),
+                    )),
               )
             ],
           ),
-        )
-    );
+        ));
   }
 }
-
 
 class BuildSections extends StatelessWidget {
   final String image;
   final String title;
   final String text;
-  const BuildSections({super.key,
-  required this.image,
-  required this.title,
-  required this.text,
+  const BuildSections({
+    super.key,
+    required this.image,
+    required this.title,
+    required this.text,
   });
 
   @override
@@ -59,30 +92,46 @@ class BuildSections extends StatelessWidget {
       width: 320,
       height: 130,
       decoration: BoxDecoration(
-          color: Colors.grey[800],
-          borderRadius: BorderRadius.circular(8.0)
-      ),
+          color: Colors.grey[800], borderRadius: BorderRadius.circular(8.0)),
       child: Padding(
         padding: const EdgeInsets.all(10.0),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             ClipRRect(
-              borderRadius: BorderRadius.circular(16.0), // Adjust the radius as needed
+              borderRadius:
+                  BorderRadius.circular(16.0), // Adjust the radius as needed
               child: Image.asset(
                 image,
                 height: 80,
-                fit: BoxFit.cover, // Ensures the image covers the given space properly
+                fit: BoxFit
+                    .cover, // Ensures the image covers the given space properly
               ),
             ),
-            const SizedBox(width: 5,),
+            const SizedBox(
+              width: 5,
+            ),
             Expanded(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  const SizedBox(height: 10,),
-                  Text(title, style: const TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.w500),),
-                  Text(text, style: const TextStyle(color: Colors.white, fontSize: 14,),)
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    title,
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500),
+                  ),
+                  Text(
+                    text,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                    ),
+                  )
                 ],
               ),
             )
