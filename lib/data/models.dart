4667f1697a9b5b443750059b670523464a22f36c
Cornelius443 <sundaycornelius55@gmail.com>
2025-02-22 11:43:07 +0100

    added laptops

--- lib/data/models.dart
+++ lib/data/models.dart
@@ -3,7 +3,7 @@ class ElectronicProduct {
   final String category;
   final String brand;
   final String description;
-  final double price;
+  final String price;
   final String color;
   final String img;
   final int likes;
@@ -113,7 +113,7 @@ final List<ElectronicProduct> smartphones = [
       likes: likes),
 ];
 
-final List<ElectronicProduct> fridge = [
+final List<ElectronicProduct> fridges = [
   ElectronicProduct(
       name: name,
       category: category,
@@ -206,7 +206,7 @@ final List<ElectronicProduct> fridge = [
       likes: likes),
 ];
 
-final List<ElectronicProduct> ac = [
+final List<ElectronicProduct> airConditioners = [
   ElectronicProduct(
       name: name,
       category: category,
@@ -299,14 +299,14 @@ final List<ElectronicProduct> ac = [
       likes: likes),
 ];
 
-final List<ElectronicProduct> smartwatch = [
+final List<ElectronicProduct> smartwatches = [
   ElectronicProduct(
       name: "Bluetooth SmartWatch",
       category: "SmartWatch",
       brand: "Apple",
       description:
           "This is a wristWatch A1 Bluetooth Smart Watch With SIM Camera",
-      price: 18965,
+      price: "18965",
       color: "black",
       img: 'assets/images/smartwatch/watch-removebg-preview.png',
       likes: 2),
@@ -316,7 +316,7 @@ final List<ElectronicProduct> smartwatch = [
       brand: "Oraimo",
       description:
           "This is a U8 Mobile Phone Watch With SIM And Memory Card Port Smart Watch",
-      price: 27000,
+      price: "27000",
       color: "black",
       img: "assets/images/smartwatch/watchtwo-removebg-preview.png",
       likes: 5),
@@ -326,7 +326,7 @@ final List<ElectronicProduct> smartwatch = [
       brand: "Oraimo",
       description:
           "This is a Z93 Pro Amoled Round Smart Watch For Android And iOS devices",
-      price: 30000,
+      price: "30000",
       color: "black",
       img: "assets/images/smartwatch/watchthree_jpeg-removebg-preview.png",
       likes: 2),
@@ -336,7 +336,7 @@ final List<ElectronicProduct> smartwatch = [
       brand: "Rolex",
       description:
           "This is a Series 8 Smartwatch For Bluetooth Calls/Messages (RoundHead)",
-      price: 25850,
+      price: "25850",
       color: "black",
       img: "assets/images/smartwatch/watchfour-removebg-preview.png",
       likes: 3),
@@ -345,7 +345,7 @@ final List<ElectronicProduct> smartwatch = [
       category: "SmartWatch",
       brand: "Oraimo",
       description: "Oraimo Nova V 2.01 HD Video Watch Faces Smart Watch ",
-      price: 980000,
+      price: "980000",
       color: "black",
       img: "assets/images/smartwatch/watchfive-removebg-preview.png",
       likes: 2),
@@ -355,7 +355,7 @@ final List<ElectronicProduct> smartwatch = [
       brand: "Apple",
       description:
           "This is an Oraimo Nova V 2.01 HD Video Watch Faces Smart Watch",
-      price: 980000,
+      price: "980000",
       color: "black",
       img: "assets/images/smartwatch/watchsix-removebg-preview.png",
       likes: 3),
@@ -365,7 +365,7 @@ final List<ElectronicProduct> smartwatch = [
       brand: "Rolex",
       description:
           "This is a  Series 8 Smartwatch Use For Bluetooth Calls/Messages",
-      price: 980000,
+      price: "980000",
       color: "black",
       img: "assets/images/smartwatch/watchseven-removebg-preview.png",
       likes: 2),
@@ -375,7 +375,7 @@ final List<ElectronicProduct> smartwatch = [
       brand: "Huawei",
       description:
           "This is a Huawei Watch Buds Smartwatch With Wireless Earphones - Black ₦ 600,000",
-      price: 45000,
+      price: "45000",
       color: "black",
       img: "assets/images/smartwatch/watcheight-removebg-preview.png",
       likes: 3),
@@ -385,7 +385,7 @@ final List<ElectronicProduct> smartwatch = [
       brand: "Oraimo",
       description:
           "Oraimo Watch Nova AM 2.01 AMOLED Screen Curved Cover Smart Watch",
-      price: 65000,
+      price: "65000",
       color: "black",
       img: "assets/images/smartwatch/watchnine-removebg-preview.png",
       likes: 2),
@@ -395,13 +395,13 @@ final List<ElectronicProduct> smartwatch = [
       brand: "Oraimo",
       description:
           "This is an Oraimo BT Call, Health Monitor watch 4Plus SMART WATCH",
-      price: 60000,
+      price: "60000",
       color: "black",
       img: "assets/images/smartwatch/watchten-removebg-preview.png",
       likes: 2),
 ];
 
-final List<ElectronicProduct> headphone = [
+final List<ElectronicProduct> headphones = [
   ElectronicProduct(
       name: name,
       category: category,
@@ -494,100 +494,102 @@ final List<ElectronicProduct> headphone = [
       likes: likes),
 ];
 
-final List<ElectronicProduct> laptop = [
+final List<ElectronicProduct> laptops = [
   ElectronicProduct(
-      name: name,
-      category: category,
-      brand: brand,
-      description: description,
-      price: price,
-      color: color,
-      img: img,
-      likes: likes),
-  ElectronicProduct(
-      name: name,
-      category: category,
-      brand: brand,
-      description: description,
-      price: price,
-      color: color,
-      img: img,
-      likes: likes),
-  ElectronicProduct(
-      name: name,
-      category: category,
-      brand: brand,
-      description: description,
-      price: price,
-      color: color,
-      img: img,
-      likes: likes),
-  ElectronicProduct(
-      name: name,
-      category: category,
-      brand: brand,
-      description: description,
-      price: price,
-      color: color,
-      img: img,
-      likes: likes),
-  ElectronicProduct(
-      name: name,
-      category: category,
-      brand: brand,
-      description: description,
-      price: price,
-      color: color,
-      img: img,
-      likes: likes),
-  ElectronicProduct(
-      name: name,
-      category: category,
-      brand: brand,
-      description: description,
-      price: price,
-      color: color,
-      img: img,
-      likes: likes),
-  ElectronicProduct(
-      name: name,
-      category: category,
-      brand: brand,
-      description: description,
-      price: price,
-      color: color,
-      img: img,
-      likes: likes),
-  ElectronicProduct(
-      name: name,
-      category: category,
-      brand: brand,
-      description: description,
-      price: price,
-      color: color,
-      img: img,
-      likes: likes),
-  ElectronicProduct(
-      name: name,
-      category: category,
-      brand: brand,
-      description: description,
-      price: price,
-      color: color,
-      img: img,
-      likes: likes),
-  ElectronicProduct(
-      name: name,
-      category: category,
-      brand: brand,
-      description: description,
-      price: price,
-      color: color,
-      img: img,
-      likes: likes),
+      name: "laptop",
+      category: "Laptop",
+      brand: "Hp",
+      description: "This is a laptop",
+      price: "100000",
+      color: "White",
+      img: "assets/images/laptops/laptop1.png",
+      likes: 3
+  ),  ElectronicProduct(
+      name: "laptop",
+      category: "Laptop",
+      brand: "Samsung",
+      description: "This is a laptop",
+      price: "200000",
+      color: "Black",
+      img: "assets/images/laptops/laptop2.png",
+      likes: 1
+  ),  ElectronicProduct(
+      name: "laptop",
+      category: "Laptop",
+      brand: "Hp",
+      description: "This is a laptop",
+      price: "100000",
+      color: "Black",
+      img: "assets/images/laptops/laptop3.png",
+      likes: 3
+  ),  ElectronicProduct(
+      name: "laptop",
+      category: "Laptop",
+      brand: "Hp",
+      description: "This is a laptop",
+      price: "100000",
+      color: "Grey",
+      img: "assets/images/laptops/laptop4.png",
+      likes: 2
+  ),  ElectronicProduct(
+      name: "laptop",
+      category: "Laptop",
+      brand: "Hp",
+      description: "This is a laptop",
+      price: "300000",
+      color: "Gold",
+      img: "assets/images/laptops/laptop5.png",
+      likes: 4
+  ),  ElectronicProduct(
+      name: "laptop",
+      category: "Laptop",
+      brand: "Dell",
+      description: "This is a laptop",
+      price: "100000",
+      color: "Black",
+      img: "assets/images/laptops/laptop6.png",
+      likes: 3
+  ),  ElectronicProduct(
+      name: "laptop",
+      category: "Laptop",
+      brand: "Acer",
+      description: "This is a laptop",
+      price: "100000",
+      color: "Black",
+      img: "assets/images/laptops/laptop7.png",
+      likes: 3
+  ),  ElectronicProduct(
+      name: "laptop",
+      category: "Laptop",
+      brand: "Macbook",
+      description: "This is a laptop",
+      price: "100000",
+      color: "White",
+      img: "assets/images/laptops/laptop8.png",
+      likes: 3
+  ),  ElectronicProduct(
+      name: "laptop",
+      category: "Laptop",
+      brand: "Macbook",
+      description: "This is a laptop",
+      price: "300000",
+      color: "Grey",
+      img: "assets/images/laptops/laptop9.png",
+      likes: 3
+  ),  ElectronicProduct(
+      name: "laptop",
+      category: "Laptop",
+      brand: "Macbook",
+      description: "This is a laptop",
+      price: "100000",
+      color: "White",
+      img: "assets/images/laptops/laptop10.png",
+      likes: 3
+  ),
+
 ];
 
-final List<ElectronicProduct> television = [
+final List<ElectronicProduct> televisions = [
   ElectronicProduct(
       name: name,
       category: category,
